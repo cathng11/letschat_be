199e30d2876819e020e1f8cf79a6d42b7a355176
Vinh Ng <cathng11@gmail.com>
2020-12-22 18:16:57 +0700

    add db

--- Controller/DB.js
+++ Controller/DB.js
@@ -3,11 +3,12 @@ var mysql = require('mysql');
 const dbRoute = express.Router();
 
 var con = mysql.createConnection({
-    user: 'root',
-    password: '',
-    database: 'chat',
-    dateStrings: true,
-    charset : 'utf8mb4'
+  host: 'sql12.freemysqlhosting.net',
+  user: 'sql12383475',
+  password: '2ufy78fXcA',
+  database: 'sql12383475',
+  dateStrings: true,
+  charset : 'utf8mb4'
 });
 con.connect(function (err) {
     if (err) throw err.stack;
